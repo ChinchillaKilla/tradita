6b58f632ccf8b5af2c774bac687afe09c73b01b6
ChinchillaKilla <romeoskalecki@gmail.com>
2025-06-02 16:18:53 +0200

    Delete js directory

--- js/modal.js
+++
@@ -1,9 +0,0 @@
-$( document ).ready(function() {
-  $('.trigger').on('click', function() {
-     $('.modal-wrapper').toggleClass('open');
-     $('.whole-page-wrap').toggleClass('blur-it');
-     return false;
-  });
-
-
-});
